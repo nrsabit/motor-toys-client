7b812a738d0962a018d3db45806a728b0604d9a8
nrsabit <mailbox.sabit@gmail.com>
2023-05-19 00:03:07 +0600

    basic routes setup completed

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { createBrowserRouter} from "react-router-dom";
+import Main from '../Layout/Main/Main';
+import Home from '../Pages/Home/Home';
+import Blogs from '../Pages/Blogs/Blogs';
+import AllToys from '../Pages/AllToys/AllToys';
+import MyToys from '../Pages/MyToys/MyToys';
+import AddToy from '../Pages/AddToy/AddToy';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Register/Register';
+import NotFound from '../Pages/NotFound/NotFound';
 
-const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: 'blogs',
+            element: <Blogs></Blogs>
+        },
+        {
+            path: 'all-toys',
+            element: <AllToys></AllToys>
+        },
+        {
+            path: 'my-toys',
+            element: <MyToys></MyToys>
+        },
+        {
+            path: 'add-toy',
+            element: <AddToy></AddToy>
+        },
+        {
+            path: 'login',
+            element: <Login></Login>
+        },
+        {
+            path: 'register',
+            element: <Register></Register>
+        }
+      ]
+    },
+    {
+        path: '*',
+        element: <NotFound></NotFound>
+    }
+  ]);
 
-export default Routes;
+export default router;
