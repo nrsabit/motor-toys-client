7742987e2aad7af46a9d69914778c2d961a845a2
nrsabit <mailbox.sabit@gmail.com>
2023-05-19 14:53:36 +0600

    login and sign up functions added

--- src/components/AuthProvider/AuthProvider.jsx
+++ src/components/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const googleAuthProvider = new GoogleAuthProvider();
+
+  const signUp = (email, password) => {
+    setLoader(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    setLoader(true);
+    return signInWithPopup(auth, googleAuthProvider);
+  };
+
+  const logInWithEmail = (email, password) => {
+    setLoader(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoader(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setLoader(false);
+      } else {
+        setUser(null);
+        setLoader(false);
+      }
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loader,
+    setLoader,
+    signUp,
+    logInWithEmail,
+    logOut,
+    signInWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
