f24844dcd36a906131ef861c013a983ff6150c67
nrsabit <mailbox.sabit@gmail.com>
2023-05-20 16:55:11 +0600

    show by numbers added

--- src/components/Pages/AllToys/AllToys.jsx
+++ src/components/Pages/AllToys/AllToys.jsx
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const AllToys = () => {
-  const toys = useLoaderData();
+  const toysPerPageOptions = [20, 15, 10, "all"];
+  const [toysPerPage, setToysPerPage] = useState(20);
+  const loadedToys = useLoaderData();
+  const [toys, setToys] = useState(loadedToys);
+
+  const handleToysPerPage = (event) => {
+    const value = event.target.value;
+    setToysPerPage(value);
+  };
+
+  useEffect(() => {
+    fetch(`https://motor-toys-server.vercel.app/all-toys?limit=${toysPerPage}`)
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, [toysPerPage]);
+
   return (
     <div className="my-16 md:mt-8 p-12 max-w-7xl mx-auto md:px-36 md:pb-12">
       <h1 className="mb-10 text-3xl md:text-5xl text-center font-bold text-gray-900">
         Explore All Toys
       </h1>
+      <div className="flex mb-8 justify-between items-center bg-base-200">
+        <div className="flex gap-3 px-5 py-3">
+          <h4>Show Toys: </h4>
+
+          <select value={toysPerPage} onChange={handleToysPerPage}>
+            {toysPerPageOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
       <div>
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-[#E0F4DB]">
             <tr>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider">
+              <th className="px-6 py-3 text-center text-xs font-bold text-gray-800 uppercase tracking-wider">
                 Image
               </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider">
+              <th className="px-6 py-3 text-center text-xs font-bold text-gray-800 uppercase tracking-wider">
                 Toy Name
               </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider">
+              <th className="px-6 py-3 text-center text-xs font-bold text-gray-800 uppercase tracking-wider">
                 Seller
               </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider">
+              <th className="px-6 py-3 text-center text-xs font-bold text-gray-800 uppercase tracking-wider">
                 Sub-Category
               </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider">
+              <th className="px-6 py-3 text-center text-xs font-bold text-gray-800 uppercase tracking-wider">
                 Price
               </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider">
+              <th className="px-6 py-3 text-center text-xs font-bold text-gray-800 uppercase tracking-wider">
                 Available Quantity
               </th>
               <th className="px-6 py-3 text-center text-xs font-medium text-gray-800 uppercase tracking-wider"></th>
